0700f753b8bbc97218f7c8774206230a36ee888a
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-10 07:01:17 +0600

    components created daisy setup primary color set

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About';
+import Reviews from './Pages/Reviews/Reviews';
+import Appointment from './Pages/Appointment/Appointment';
+import ContactUs from './Pages/ContactUs/ContactUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/reviews' element={<Reviews />}></Route>
+        <Route path='/appoinment' element={<Appointment />}></Route>
+        <Route path='/contact   ' element={<ContactUs />}></Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+ 
