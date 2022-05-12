edd52edc85315f1c815f85c3732055da61f833a9
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-12 07:16:04 +0600

    validation added and spinned added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
+import Loading from '../Shared/Loading';
 
 const Login = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
   const { register, formState: { errors }, handleSubmit } = useForm();
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  let signInError;
+
+  if (loading || gLoading) {
+    return <Loading />
+  }
+
+  if (error || gError) {
+    signInError = <p className='text-red-500'><small>{error?.message || gError?.message}</small></p>
+  }
+
+
+  if (gUser) {
+    console.log(gUser);
+  }
+
   const onSubmit = data => {
-    console.log(data)
+    console.log(data);
+    signInWithEmailAndPassword(data.email, data.password);
+
   };
 
-  if (user) {
-    console.log(user);
-  }
+
 
 
   return (
@@ -71,6 +95,8 @@ const Login = () => {
                 {errors.password?.type === 'minLength' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
               </label>
             </div>
+
+            {signInError}
 
             <input class="btn w-full max-w-xs text-white" type="submit" value="Login" />
           </form>
---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='flex justify-center items-center h-screen w-full'>
+            <div class="w-16 h-16 border-b-4 border-gray-900 rounded-full animate-spin"></div>
+        </div>
+    );
+};
+
+export default Loading;
