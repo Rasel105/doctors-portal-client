3230c938bb2da05c440ea39c8f07094c64606910
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-15 19:58:34 +0600

    verify token and singout the user and remove the token

--- src/Pages/Dashboard/MyAppoinment.js
+++ src/Pages/Dashboard/MyAppoinment.js
@@ -1,19 +1,36 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const MyAppoinment = () => {
     const [appointment, setAppoinment] = useState([]);
     const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate();
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/booking?patientEmail=${user.email}`)
-                .then(res => res.json())
-                .then(data => setAppoinment(data));
+            fetch(`http://localhost:5000/booking?patientEmail=${user.email}`, {
+                method: "GET",
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem("accessToken")}`
+                }
+            })
+                .then(res => {
+                    console.log("Res", res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem("accessToken");
+                        navigate("/");
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    setAppoinment(data)
+                });
         }
-    }, [user])
-    console.log(appointment);
-
+    }, [user, navigate])
 
     return (
         <div>
