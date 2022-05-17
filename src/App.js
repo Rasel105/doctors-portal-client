6ebe00d919904296d301fc98854beea2ab26f0d2
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-17 08:16:16 +0600

    doctors data show in manage doctors components

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import ForgetPassword from './Pages/Login/ForgetPassword'
 import Users from './Pages/Dashboard/Users';
 import RequireAdmin from './Pages/Login/RequireAdmin';
 import AddDoctor from './Pages/Dashboard/AddDoctor'
+import ManageDoctor from './Pages/Dashboard/ManageDoctor';
 function App() {
   return (
     <div className='max-w-7xl	mx-auto px-12'>
@@ -43,6 +44,7 @@ function App() {
           <Route path='myhistory' element={<Myhistory />}></Route>
           <Route path='users' element={<RequireAdmin><Users /></RequireAdmin>}></Route>
           <Route path='addDoctor' element={<RequireAdmin><AddDoctor /></RequireAdmin>}></Route>
+          <Route path='manageDoctor' element={<RequireAdmin><ManageDoctor /></RequireAdmin>}></Route>
         </Route>
         <Route path='/contact' element={<ContactUs />}></Route>
         <Route path='/login' element={<Login />}></Route>
