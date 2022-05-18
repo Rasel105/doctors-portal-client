7db1d2098a5e9f8f8c368e08c27e01f995c3ba21
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-18 18:06:33 +0600

    payent completed message showed

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,48 +1,112 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ appoinment }) => {
     const stripe = useStripe();
     const elements = useElements();
+    const [cardError, setCardError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [transactionId, setTransactionId] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const { price, patientEmail, patient } = appoinment;
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/create-payment-intent`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem("accessToken")}`
+            },
+            body: JSON.stringify({ price }),
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret);
+                }
+            })
+    }, [price])
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        };
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: "card",
+            card,
+        });
+
+
+        setCardError(error?.message || '')
+        setSuccess("");
+        // confirm card payment 
+
+        const { paymentIntent, error: ententError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patient,
+                        email: patientEmail
+                    },
+                },
+            },
+        );
+        if (ententError) {
+            setCardError(ententError.message);
+        } else {
+            setCardError("");
+            console.log(paymentIntent)
+            setTransactionId(paymentIntent.id)
+            setSuccess("Congrats! Your payment is completed.")
+        }
     }
-
-    if (!stripe || !elements) {
-        return;
-    }
-
-    const card = elements.getElement(CardElement);
-    
-    if (card == null) {
-        return;
-    }
-
-
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+            </form>
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
+            {
+                success && <div className='text-green-500'>
+                    <p>{success}</p>
+                    <p>Your transaction Id: <span className='text-orange-500 font-bold'>{transactionId}</span></p>
+                </div>
+            }
+        </>
     );
 };
 
